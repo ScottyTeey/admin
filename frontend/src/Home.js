f5ab56a5499fc8fd94ade21a47183ddf3d729051
lebogang <janniejr55@gmail.com>
2023-09-13 16:04:22 +0200

    homepage

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -4,7 +4,7 @@ export class Home extends Component{
     render(){
         return(
             <div>
-                <h3>This is home page</h3>
+                <h3>This is home page changed</h3>
             </div>
         )
     }
